b5133d4b512af7046dd5ced774cf8248fa2d00eb
Naleo <naleo@naleo.me>
2022-05-18 20:46:16 -1000

    Setup base app w/ connection to dockerized mongodb

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const mongoose = require('mongoose');
+//require statements
+const express = require("express");
+const bodyParser = require("body-parser");
 
-main().catch(err => console.log(err));
+const mongoose = require("mongoose");
 
-async function main() {
-  await mongoose.connect('mongodb://172.18.0.2:27017/test')
-}
+//allow for .env file usage
+require("dotenv").config();
+
+const db = process.env.MONGO_URI || "mongodb://localhost:27017/dev";
+const port = process.env.PORT || 4000;
+
+mongoose.connect(db).then(() => {
+  const app = express();
+  app.listen(port, () => {
+    console.log("Server has started!");
+  });
+});
