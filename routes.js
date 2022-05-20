e37da2dfcbca4ec346b5c48ef8efec19c6e8f16e
Naleo <naleo@naleo.me>
2022-05-19 23:46:25 -1000

    Add route PATCH update Course

--- routes.js
+++ routes.js
@@ -35,5 +35,51 @@ router.post("/courses", async (req, res) => {
     res.send(course);
 });
 
+router.patch("/courses/:id", async (req, res) => {
+    try {
+        const course = await Course.findOne({ _id: req.params.id });
+        
+        if (req.body.studentId) {
+            course.studentId = req.body.studentId;
+        }
+
+        if (req.body.courseId) {
+            course.courseId = req.body.courseId;
+        }
+        
+        if (req.body.dateGiven) {
+            course.dateGiven = req.body.dateGiven;
+        }
+
+        if (req.body.dateDue) {
+            course.dateDue = req.body.dateDue;
+        }
+        
+        if (req.body.isComplete) {
+            course.isComplete = req.body.isComplete;
+        }
+
+        if (req.body.dateComplete) {
+            course.dateComplete = req.body.dateComplete;
+        }
+
+        if (req.body.pointsPossible) {
+            course.pointsPossible = req.body.pointsPossible;
+        }
+
+        if (req.body.pointsEarned) {
+            course.pointsEarned = req.body.pointsEarned;
+        }
+
+        if (req.body.description) {
+            course.description = req.body.description;
+        }
+        await course.save();
+        res.send(course);
+    } catch {
+        res.status(404);
+        res.send({ error: "Course does not exist!" });
+    }
+});
 
 module.exports = router;
