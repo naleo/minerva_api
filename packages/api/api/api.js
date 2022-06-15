ed9bb073e080820c18a0652c8e502cf1742e2ef0
Naleo <naleo@naleo.me>
2022-06-14 19:53:36 -1000

    Add frontend app outline and tailwindcss

--- packages/api/api/api.js
+++ packages/api/api/api.js
@@ -8,7 +8,7 @@ const authRoutes = require("./routes/auth.routes");
 
 //allow for .env file usage
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 
 const app = express();
